b005db1579dfb1a86f944584d3ec51cac9cdbfb2
Brajbrishti <brajendrasinghdee17@gmail.com>
2025-04-23 16:42:27 +0530

    comment added

--- TC-1.py
+++ TC-1.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 import select
 driver = googlechrome()
-
+### code starts from here ###
 driver.get("http://www.facebook.com")
 driver.maximize_window()
 driver.find_element(By.XPATH,'//input[@name="email"]').send_keys("brajendra")
