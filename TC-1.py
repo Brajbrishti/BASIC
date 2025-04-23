433a5be750da620d2ab5f8337e50af7111055228
Brajbrishti <brajendrasinghdee17@gmail.com>
2025-04-23 16:36:15 +0530

    new driver added

--- TC-1.py
+++ TC-1.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 import time
 import select
-driver = Chrome()
+driver = googlechrome()
 driver.get("http://www.facebook.com")
 driver.maximize_window()
 driver.find_element(By.XPATH,'//input[@name="email"]').send_keys("brajendra")
