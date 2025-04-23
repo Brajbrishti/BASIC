df1b6ca01e26dd8b28756e3094592ec0beac78a8
Brajbrishti <brajendrasinghdee17@gmail.com>
2025-04-23 15:04:51 +0530

    Code has changed

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+
 class A:
-    def Number_print():
+    def Number_ser(self):
         for i in range(1,15,2):
             print(i)
+    def addition(self,num1,num2):
+        num1=int(input("enter num1"))
+        num2=int(input("enter num2"))
+        print("result is :",num1 + num2)
+
+obj=A()
+obj.addition(15,45)
+obj.Number_ser()
