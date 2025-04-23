cdc653fbd3fa08472db82c53498f4b69f2a9c1c6
Brajbrishti <brajendrasinghdee17@gmail.com>
2025-04-23 16:30:58 +0530

    changes

--- TC-1.py
+++ TC-1.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 import time
 import select
 driver = Chrome()
-#act = ActionChains(driver)
 
 driver.get("http://www.facebook.com")
 driver.maximize_window()
