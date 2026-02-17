7f6cb00ad91eb40197ea33be27b2479968ba0d2a
brajendra <154320255+Brajbrishti@users.noreply.github.com>
2026-02-17 17:21:27 +0530

    Update test_facebook.py

--- test_facebook.py
+++ test_facebook.py
@@ -7,6 +7,7 @@ driver = Chrome()
 ### code starts from here ###
 driver.get("http://www.facebook.com")
 driver.maximize_window()
+time.sleep(3)
 driver.find_element(By.XPATH,'//input[@name="email"]').send_keys("brajendra")
 time.sleep(3)
 driver.close()
