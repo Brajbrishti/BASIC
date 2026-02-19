832085673ec7a7333071af2a47e3ec2938894697
Brajbrishti <brajendrasinghdee17@gmail.com>
2026-02-19 14:57:28 +0530

    inheritance method

--- test.py
+++ test.py
@@ -3,19 +3,24 @@ class A:
     def Number_ser(self):
         for i in range(1,15,2):
             print(i)
+        
+class B(A):
+    def __int__(self):
+        print("Child class")
+
     def multiplication(self,a,b):
         result=a*b
         print("result",result)        
-
+class C(B,A):
+    
     def addition(self,num1,num2):
         num1=int(input("enter num1"))
         num2=int(input("enter num2"))
         print("result is :",num1 + num2)
-class B(A):
-    def __int__(self):
-        print("Child class")
-obj=B()
-obj.addition(1,2)
-obj.multiplication(15,15)
 
+
+obj=C()
+obj.multiplication(5,2)
+obj.Number_ser()
+obj.addition(50,20)
 
